6064703ef28ed7d4d9a0896cf7299516e1bcd5c0
Santiago Gonzalez Toral <hernangt12re3@gmail.com>
2019-01-04 00:17:29 -0500

    Add i18n for localization

---
+++ src/i18n/de.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"app.name": "Influme",
+	"company.name": "Ihr Firmenname",
+	"button": {
+		"login": "Einloggen",
+		"logout": "Ausloggen",
+	},
+	"welcome": "Herzlich willkommen",
+	"home": "Zuhause",
+	"news": "Nachrichten",
+	"favorites": "Favoriten",
+	"settings": "die Einstellungen",
+};
---
+++ src/i18n/en.default.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"app.name": "Influme",
+	"company.name": "Influme",
+	"button": {
+		"login": "Log in",
+		"logout": "Log out",
+	},
+	"welcome": "Welcome",
+	"home": "Home",
+	"news": "News",
+	"favorites": "Favorites",
+	"settings": "Settings",
+};
