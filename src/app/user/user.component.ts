15b84c9bfafc3832745642ab8323411a58f013d4
Renan <renangfz@hotmail.com>
2019-03-15 10:19:56 -0500

    Creation and editing of user profile and get location

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,6 +11,15 @@ import * as localstorage from "nativescript-localstorage";
 
 import { UserapiService } from "../shared/api/user/userapi.service";
 
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
+
+import { request, getFile, getImage, getJSON, getString } from "tns-core-modules/http";
+
+const localize = require("nativescript-localize");
+// import * as request from "request-promise-native"
+
+
 
 @Component({
   selector: 'user',
@@ -25,31 +34,58 @@ export class UserComponent implements OnInit {
   private imageurl: string; 
   private UserLogData: any;
   private lcity: string;
-  // private jsonDataUser: any;
+  private label_button: string;
+  private showDetails: string;
+  private menuOption: any;
+  
+  locations = [];
 
 
   constructor(private route: ActivatedRoute, private _routerExt: RouterExtensions, private page: Page,
                 private userApiService: UserapiService) { 
 
-	this.page.actionBarHidden = true;
- //  this.page.backgroundSpanUnderStatusBar = true;
-        
-  // this._routerExtensions.params.forEach((params) => { this.token = params["info"]; });
-  // this.route.queryParams.subscribe(params => { this.token = params["info"]; });
+      this.page.actionBarHidden = true;
 
-  let infoUser = localStorage.getItem('ResultLogin');
+      this.enableLocationTap();
+      this.buttonGetLocationTap();
 
-  this.UserLogData = JSON.parse(infoUser);
+      this.menuOption = -1;
+
+      this.route.queryParams.subscribe(params => {
+          this.menuOption = params["menuOption"];
+      });
+
+      if(this.menuOption == 1){
+        this.label_button = localize("edit");
+        this.showDetails = "collapsed";
+
+      }else if(this.menuOption == 0){
+        this.label_button = localize("button.continue");
+        this.showDetails = "visible";
+
+      }
+
+      //  this.page.backgroundSpanUnderStatusBar = true;    
+      // this._routerExtensions.params.forEach((params) => { this.token = params["info"]; });
+      // this.route.queryParams.subscribe(params => { this.token = params["info"]; });
+
+      let infoUser = localstorage.getItem('ResultLogin');
+
+      // if(infoUser == null){
+      //     infoUser = localStorage.getItem('ResultLogin');         
+      // }
+
+      this.UserLogData = JSON.parse(infoUser);
        
-   const vm = new Observable();
+      const vm = new Observable();
       //this.lnname = UserLogData["nickname"];
       //console.log('entry');
       //console.log(jwt);
     	// const usuario: string = jwt(this.token['idToken']);
-     //  console.log("luego del decode");
+      //  console.log("luego del decode");
     	// console.log (JSON.stringify(usuario));
     	//vm.set("imageurl", usuario['picture']);
-	vm.set("lnname", this.UserLogData["nickname"]);
+	    vm.set("lnname", this.UserLogData["nickname"]);
     	//vm.set("lname", usuario['name']);
     	
       //this.page.bindingContext = vm;   
@@ -60,34 +96,32 @@ export class UserComponent implements OnInit {
 
       //StackLayout.bindingContext = vm;
 
-  	this.lnname = this.UserLogData["nickname"];
-  	this.lname = this.UserLogData["name"];
-  	this.imageurl = this.UserLogData["pictureURL"];
+    	this.lnname = this.UserLogData["nickname"];
+    	this.lname = this.UserLogData["name"];
+    	this.imageurl = this.UserLogData["pictureURL"];
 
-    // this.jsonDataUser = {
-    //   "nameU": this.lname,
-    //   "cityU": "Cuenca, Ecuador",
-    //   "imageU": this.imageurl
-    // }
+      // this.jsonDataUser = {
+      //   "nameU": this.lname,
+      //   "cityU": "Cuenca, Ecuador",
+      //   "imageU": this.imageurl
+      // }
 
-    this.UserLogData["name"] = this.lname;
-    this.UserLogData["pictureURL"] = this.imageurl;
-    this.UserLogData["city"] = this.lcity;
+      this.UserLogData["name"] = this.lname;
+      this.UserLogData["pictureURL"] = this.imageurl;
+      this.UserLogData["city"] = this.lcity;
 
-    // this.UserLogData = {
-    //   "name": result.name,
-    //   "nickname": result.nickname,
-    //   "pictureURL": result.pictureURL,
-    //   "city": "",
-    //   "accessToken": res['accessToken'],
-    //   "idToken": res['idToken']
+      // this.UserLogData = {
+      //   "name": result.name,
+      //   "nickname": result.nickname,
+      //   "pictureURL": result.pictureURL,
+      //   "city": "",
+      //   "accessToken": res['accessToken'],
+      //   "idToken": res['idToken']
 
-
-
-    //   "nameU": this.lname,
-    //   "cityU": "Cuenca, Ecuador",
-    //   "imageU": this.imageurl
-    // }
+      //   "nameU": this.lname,
+      //   "cityU": "Cuenca, Ecuador",
+      //   "imageU": this.imageurl
+      // }
 
   }
 
@@ -96,7 +130,6 @@ export class UserComponent implements OnInit {
 
   continue() {
       this.navigateInterest();
-      console.log("aqui continue");
   }
 
   goback(){
@@ -114,6 +147,67 @@ export class UserComponent implements OnInit {
       //   }
       // };      
       // this._routerExt.navigate(["interest"], navigationExtras);
-      this._routerExt.navigate(["interest"]);
+      if(this.menuOption == 0){
+        let editOption = 0;
+        let navigationExtras: NavigationExtras = {
+        queryParams: {
+              "menuOption": editOption
+        }};
+        
+        this._routerExt.navigate(["interest"], navigationExtras);
+      }else if(this.menuOption == 1){
+        this._routerExt.navigate(["profile"]);
+      }
     }
+
+  public enableLocationTap() {
+      geolocation.isEnabled().then(function (isEnabled) {
+          if (!isEnabled) {
+              geolocation.enableLocationRequest().then(function () {
+              }, function (e) {
+                  console.log("Error: " + (e.message || e));
+              });
+          }
+      }, function (e) {
+          console.log("Error: " + (e.message || e));
+      });
+  }
+
+  public buttonGetLocationTap() {
+      // let that = this;
+      geolocation.getCurrentLocation({
+          desiredAccuracy: Accuracy.high,
+          maximumAge: 5000,
+          timeout: 10000
+      }).then(function (loc) {
+          if (loc) {
+
+            getJSON("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + loc.latitude + "," + loc.longitude + "&key=AIzaSyBD71DgYMShdu8PD0x7waGLBPBgZjAZKes").then((r: any) => {
+              console.log(JSON.stringify(r));
+            }, (e) => {
+              console.log("Error" + e);
+            })
+              // let options = {
+              //     uri: "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + loc.latitude + "," + loc.longitude + "&sensor=true",
+              // };
+              // const result = await request.get(options);
+              // request.get(options).then((result: string) => console.log(result));
+              // console.log("{****} ADDRESS: " + result);
+            // let url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + loc.latitude + "," + loc.longitude + "&sensor=true";
+            // this.http.get(url)
+            //     .map(results => results.json())
+            //     .subscribe(results => {
+            //             console.log("address " + results.results[0].formatted_address);
+            //     }, error => {
+            //         console.log("ERROR: ", error);
+            //     });  
+
+            console.log("Location: "+ JSON.stringify(loc));
+            // that.locations.push(loc);
+          }
+      }, function (e) {
+          console.log("Error: " + (e.message || e));
+      });
+  }
+
 }
