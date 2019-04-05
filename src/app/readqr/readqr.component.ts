728600af485274160d524de20fc176841c2c6fa4
Renan <renangfz@hotmail.com>
2019-04-04 22:16:32 -0500

    Register deals per user

--- src/app/readqr/readqr.component.ts
+++ src/app/readqr/readqr.component.ts
@@ -6,8 +6,15 @@ import { ActivatedRoute } from "@angular/router";
 import { NavigationExtras } from "@angular/router";
 import { Page } from "tns-core-modules/ui/page";  
 
+import { UsersdealsService } from "../shared/api/usersdeals/usersdeals.service";
+import { DealsprofileService } from "../shared/api/dealsprofile/dealsprofile.service";
+
 import { Markerprofile } from "../shared/models/markerprofile.model";
 import { Dealsprofile } from "../shared/models/dealsprofile.model";
+import { Usersdeals } from "../shared/models/usersdeals.model";
+
+
+
   
 
 @Component({
@@ -20,9 +27,12 @@ export class ReadqrComponent implements OnInit {
 
   deal_profile: Dealsprofile[];
   marker_profile: Markerprofile;
+  userIdentification: string = "5c96f09a6d69fdd962e49c19";
 
 
-  constructor(private barcodeScanner: BarcodeScanner, private _routerExtensions: RouterExtensions, private route: ActivatedRoute, private page: Page) { 
+  constructor(private barcodeScanner: BarcodeScanner, private _routerExtensions: RouterExtensions, 
+    private route: ActivatedRoute, private page: Page, private usersdealsService: UsersdealsService,
+    private dealsprofileService: DealsprofileService) { 
         
     this.page.actionBarHidden = true;
     // this.page.backgroundSpanUnderStatusBar = true;
@@ -46,26 +56,46 @@ export class ReadqrComponent implements OnInit {
         message: "Congratulations!!, the discount is yours",
         okButtonText: "Close"
     }).then(() => {
+      
       this.barcodeScanner.stop();
-      // this._routerExtensions.navigate(["markerprofile"], {
-      //     clearHistory: true,
-      //     animated: true,
-      //     transition: {
-      //     name: "slideRight",
-      //     duration: 350,
-      //     curve: "ease"
-      //     }
-      //  });
+      
+      let objectUsersDeals = {} as Usersdeals;
+      objectUsersDeals.userid = this.userIdentification;
+      objectUsersDeals.dealid = this.deal_profile[0]._id;
 
-      let navigationExtras: NavigationExtras = {
-        queryParams: {
-            "MarkerProfile": JSON.stringify(this.marker_profile)
-          }
-      };
-      this._routerExtensions.navigate(["markerprofile"], navigationExtras)
+      //save user-deal
+      this.postUserDeal(objectUsersDeals).then(dataResponse => {
 
-        console.log("Dialog closed!");
+        let objectTickets = {} as Dealsprofile;
+        objectTickets.used_tickets = this.deal_profile[0].used_tickets + 1 ;
+
+        console.log("[**] Update Deals Ticekts " + JSON.stringify(objectTickets));
+        
+        //update number used_tickets
+        this.updateUsedTickets(this.deal_profile[0]._id, objectTickets).then(dataResponseDeal => {
+
+          console.log("[**] Response Update Ticekts " + JSON.stringify(dataResponseDeal));
+
+          this.deal_profile[0].used_tickets = dataResponseDeal.used_tickets;         
+
+          let navigationExtras: NavigationExtras = {
+            queryParams: {
+                "MarkerProfile": JSON.stringify(this.marker_profile)
+              }
+          };
+          // this._routerExtensions.navigate(["markerprofile"], navigationExtras)
+          this._routerExtensions.navigate(["viewmap"], {clearHistory: true});
+          // this._routerExtensions.back();
+
+
+          console.log("Dialog closed!");
+        });
+
+      });
+
     });
+
+
   }
 
   public scanTapped(): void {
@@ -96,4 +126,32 @@ export class ReadqrComponent implements OnInit {
         });
     }	
   }
+
+
+  async postUserDeal(objectUpdate: Usersdeals) {
+
+      try {
+          const user_deal: Usersdeals = await this.usersdealsService.saveDealUser(objectUpdate);
+          // var dealsprofilecontent: any = JSON.parse(deals_profile); 
+          return user_deal;
+      } catch(err) {
+          console.log(err);
+      }
+      
+  }
+
+  async updateUsedTickets(dealId, objectUpdate: Dealsprofile) {
+
+      try {
+
+          const tickets_deal_update: Dealsprofile = await this.dealsprofileService.updateTicketsForDeal(dealId, objectUpdate);
+          // var dealsprofilecontent: any = JSON.parse(deals_profile); 
+          return tickets_deal_update;
+      } catch(err) {
+          console.log(err);
+      }
+      
+  }  
+
+
 }
