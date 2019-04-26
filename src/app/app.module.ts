c3cf4236a59a83841415af702f7e52fbd3444e92
Renan <renangfz@hotmail.com>
2019-04-26 00:28:32 -0500

    Added data provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppComponent } from "./app.component";
 
 import { LoggedInLazyLoadGuard } from "./logged-in-lazy-load.guard";
 
+import { Data } from "./providers/data/data";
+
+
 @NgModule({
     bootstrap: [
         AppComponent
@@ -23,7 +26,8 @@ import { LoggedInLazyLoadGuard } from "./logged-in-lazy-load.guard";
         AppComponent
     ],
     providers: [
-        LoggedInLazyLoadGuard
+        LoggedInLazyLoadGuard,
+        Data
     ],
     schemas: [
         NO_ERRORS_SCHEMA
---
+++ src/app/providers/data/data.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Data {
+
+    public storage_vara: any;
+    public storage_varb: any;
+
+    public constructor() { }
+
+}
